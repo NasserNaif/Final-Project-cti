5fa583525e81c2273edf3340c6bbed4ddfae2fde
Nasser Naif <nmaif7788@hotmail.com>
2024-04-27 23:43:48 +0300

    create front end pages

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegistrationComponent } from './Pages/registration/registration.component';
+import { HeaderComponent } from './Pages/header/header.component';
+import { FooterComponent } from './Pages/footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent, title: 'Home'},
+  {path:'header', component: HeaderComponent, title: 'header'},
+  {path: 'login', component: LoginComponent, title: 'Login'},
+
+  {path:'registration', component: RegistrationComponent, title: 'Registration'},
+  {path:'footer', component: FooterComponent, title: 'Footer'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Pages/header/header.component';
+import { FooterComponent } from './Pages/footer/footer.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegistrationComponent } from './Pages/registration/registration.component';
+import { UpdateComponent } from './Pages/update/update.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UpdateComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterOutlet,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
